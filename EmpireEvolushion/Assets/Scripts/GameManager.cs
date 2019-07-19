20236d3dfb9ac7f6f6b0476881b33aa18847d5ae
Alex <dn070291dag@gmail.com>
2019-07-19 16:40:02 +0300

    First commit

---
+++ EmpireEvolushion/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+	public Image[] progressBar;
+	public Sprite loadingIcon;
+	public Sprite loadingDoneIcon;
+	public GameObject _prefab;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		StartCoroutine(LoadSavedScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public IEnumerator LoadSavedScene()
+	{
+		int i = 0;
+		
+		while (true)
+		{
+			progressBar[i].sprite = loadingDoneIcon;
+
+			i++;
+
+			if (i == progressBar.Length)
+			{
+				Instantiate(_prefab, new Vector3(Random.Range(-8f, 8f), Random.Range(-3.5f, 1.5f), 0), Quaternion.identity);
+				i = 0;
+				for (int j = 0; j < progressBar.Length; j++)
+				{
+					progressBar[j].sprite = loadingIcon;
+				}
+			}
+
+			yield return new WaitForSeconds(0.5f);
+		}
+		yield return null;
+	}
+}
